b407743680b22abb6454a3f605b4175060c6fe19
John Sebastián Cortés Moreno - Ceiba Software <john.cortes@ceiba.com.co>
2021-08-30 15:25:50 -0500

    correccion en las pruebas

--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -4,3 +4,6 @@ values ('12345',2L,now(),now(),800000.0);
 
 insert into finca (nombre, direccion, fecha_creacion, precio, cantidad_habitaciones)
 values ('el refugio','morelia','2020-01-02',400000.0,5);
+
+insert into finca (nombre, direccion, fecha_creacion, precio, cantidad_habitaciones)
+values ('la moravia','alta gracia','2020-02-02',450000.0,5);
